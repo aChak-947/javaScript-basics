d9c91bd8762a516cdff3661bdb77985d68acad68
Avronil Chakraborty <avronilchakraborty947@gmail.com>
2021-11-29 23:54:00 +0530

    first commit

---
+++ cp.js
@@ -0,0 +1,7 @@
+const cp = require('child_process')
+
+cp.execSync('open "/Applications/Google Chrome.app/https://www.pepcoding.com/"')
+//type in name of app in this path to open
+
+// let output = cp.execSync('node test.js')
+// console.log('' + output)
---
+++ fs.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = require('path')
+
+//reading from a file
+// let content = fs.readFileSync('f1.txt')
+// console.log('This is f1 data -> ' + content)
+
+//writing to a file
+// fs.writeFileSync('f2.txt', 'Hello from f2')
+
+//append to a file
+// fs.appendFileSync('f2.txt', ' This is f2 data.')
+
+//delete a file
+// fs.unlinkSync('f2.txt')
+// console.log('File Removed')
+
+//create a directory
+fs.mkdirSync('myDirectory')
+
+//delete a directory
+//fs.rmdirSync('myDirectory')
+
+let doesExist = fs.existsSync('myDirectory')
+console.log(doesExist)
